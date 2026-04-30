dfbb681eec4c40f45099497be5fe91c1e6dc4ccf
marcosilvroc <marco.silvroc@gmail.com>
2026-04-29 23:31:23 -0300

    Setup for Github Pages

--- src/main.py
+++ src/main.py
@@ -1,16 +1,33 @@
+import sys
+
 from copystatic import sync_static
 from gencontent import generate_pages_recursive
 
 
 def main():
     source_path = "./static"
-    destination_path = "./public"
+    # destination_path = "./public" #local testing
+    destination_path = "./docs"
     template_path = "./template.html"
     content_path = "./content"
 
-    sync_static(source_path, destination_path)
-    print("Generating html page")
-    generate_pages_recursive(content_path, template_path, destination_path)
+    try:
+        if not sys.argv[1:]:
+            basepath = "/"
+        else:
+            basepath = sys.argv[1]
+
+        sync_static(source_path, destination_path)
+        print("Generating html page")
+        generate_pages_recursive(
+            content_path,
+            template_path,
+            destination_path,
+            basepath,
+        )
+    except Exception as e:
+        print(f"Error: {e}")
+        exit(1)
 
 
 main()
